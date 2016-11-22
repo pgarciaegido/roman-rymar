c79853412320f0388aaa18d33458d4a5b90a5bc2
Pablo Garcia Egido <pgarciaegido@gmail.com>
2016-11-22 20:41:59 +0100

    Sticky nave, magellan navigation and few fixes

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,39 @@
 $(document).foundation();
+
+/*
+STICKY HEADER
+*/
+function stickyHeader(){
+	if ($(this).scrollTop() > 1){  
+    $('#navbar-container').addClass("sticky");
+  }
+  else{
+    $('#navbar-container').removeClass("sticky");
+  }
+}
+
+/*
+ANCHOR SLIGHTLY ABOVE THE BEGGINING OF DIV WHEN CLICKING MENU
+*/
+function offsetAnchor() {
+  if(location.hash.length !== 0) {
+    window.scrollTo(window.scrollX, window.scrollY - 100);
+  }
+}
+
+/*
+CLOSE OFF CANVAS MENU WHEN CLICKING AN ANCHOR
+*/
+function closeOffCanvas(ev){
+	$('#offCanvas').foundation('close');
+	// setTimeout(function(){
+	// 	$sticky.css('left', '0');
+	// }, 300);
+}
+
+/*
+EVENT HANDLERS
+*/
+$(window).scroll(stickyHeader);
+window.addEventListener("hashchange", offsetAnchor);
+$('.exit-off-canvas').on('click', closeOffCanvas);
